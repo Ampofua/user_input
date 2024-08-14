d53414961cf195ddfcb7a521e6d41eb2a09dd9fb
Ampofua <adwoaampofua@gmail.com>
2024-08-14 18:28:41 +0000

    first commit

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Kindly tell me your name: ")
+age = int(input("How old are you?"))
+location = input("Which country are you from?")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
